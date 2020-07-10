0e80568e1d9a403ef3eb7559052db2e6a1de2969
kovalev.d <kacevnik@yandex.ru>
2020-07-10 14:12:46 +0400

    start application

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import { Context } from '../context';
+
+document.oncontextmenu = function () { return false };
+
+function App() {
+
+  const Obj = window.Obj.data
+  
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
